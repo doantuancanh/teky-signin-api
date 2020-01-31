39ed3ccc3d0b63c6f24706099887a1f0e70d057d
doantuancanh94 <doantuancanh94@gmail.com>
2020-01-31 11:30:16 +0700

    init app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+	  get 'sign_in', to: 'user/sessions#new'
+	end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+end
