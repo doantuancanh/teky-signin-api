81d4a017b72cce8dbe49c9b984f35d764c50410f
doantuancanh94 <doantuancanh94@gmail.com>
2020-02-05 09:17:22 +0700

    login_api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,36 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_action :authenticate_user!, :unless => :custom_authenticate_user!
+  before_action :forgery_protection_origin_check
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit :email, :password
+    end
+  end
+
+  def custom_authenticate_user!
+    if request.headers['Authorization'].present?
+      user = User.where(:session_token => request.headers['Authorization']).last
+      if user 
+        current_user = user
+        return true
+      else
+      end
+    end
+    false
+  end
+
+  def check_sign_in
+    @user = User.where(:session_token => request.headers['Authorization']).last
+    if @user.blank?
+      render json: { message: "You need sign in first" }, status: 400
+      return
+    end
+    @user
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,59 @@
 # frozen_string_literal: true
+require 'jwt'
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :check_sign_in, except: :create
+  skip_before_action :verify_signed_out_user, only: :destroy
+  # before_action :authenticate_user!, only: :destroy
   # GET /resource/sign_in
   # def new
   #   super
   # end
-
+  after_action :reset_session_token, only: :destroy
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.where(:email => params[:user][:email]).first
+    if user && user.valid_password?(params[:user][:password])
+      token = generate_token user
+      render json: {token: token, status: :ok}, status: :ok
+      return
+    else
+      render json: {message: 'Wrong username or password'}, status: 400
+      return
+    end
+  end
+
+  def change_password
+    # if request.headers['Authorization'].present?
+      # user = User.where(:session_token => request.headers['Authorization']).last
+      if @user.valid_password?(params[:password]) && params[:new_password] == params[:renew_password]
+        @user.update(password: params[:new_password])
+        render json: {message: "Change password success"}, status: 200
+      else
+        render json: { message: "Something wrong! Try again"}, status: 400
+      end
+    # end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    @user.update(:session_token => '')
+    render json: :ok, status: 200
+  end
 
-  # protected
+  private
+  def generate_token user
+    data = {
+      iat: Time.now.to_i,
+      exp: 1.weeks.from_now.to_i,
+      sub: user.id
+    }
+    token = JWT.encode(data, 'secret', 'HS256')
+    user.update(session_token: token)
+    token
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def user_params
+    params.require(:user).permit :email, :password
+  end
 end
